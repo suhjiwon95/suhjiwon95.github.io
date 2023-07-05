dc736f743135f73841b1884909e887f52b624fd3
Jiwon Suh <suhjiwon1995@gmail.com>
2023-07-05 13:32:55 -0400

    finished mobile CSS

--- age-calculator-app-main/src/script.js
+++ age-calculator-app-main/src/script.js
@@ -1,12 +1,23 @@
 const dayInput = document.getElementById('day-input');
-const monthInput = document.getElementById('month-input').value;
-const yearInput = document.getElementById('year-input').value;
+const monthInput = document.getElementById('month-input');
+const yearInput = document.getElementById('year-input');
 const button = document.getElementById('button');
+const error = document.getElementsByClassName('error');
+const yearCalculated = document.getElementById('year-calculated');
 
 const getBday = () => {
 
-    const day = new Date(dayInput.vaalue);
-    console.log(day);
+    const today = new Date();
+    const todayMoment = moment(today);
+    const birthday = new Date(yearInput.value, monthInput.value - 1, dayInput.value);
+    const bdayMoment = moment(birthday);
+    const difference = todayMoment.from(bdayMoment);
+  
+    console.log();
+    yearCalculated.textContent = difference;
+
+    
+
 
     
 }
