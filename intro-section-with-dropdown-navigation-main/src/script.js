7401349e00de1630c906f1d2a2669a2bad30c402
Jiwon Suh <suhjiwon1995@gmail.com>
2023-08-02 14:32:26 -0400

    added basic js

---
+++ intro-section-with-dropdown-navigation-main/src/script.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+});
+
+document.querySelectorAll(".dropdown-content li").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove('active');
+    nav.classList.remove('active');
+}))
+
+
+// JavaScript to show/hide dropdown content when clicking on ".menu-header"
+    const headers = document.querySelectorAll('.menu-header');
+
+    headers.forEach(header => {
+      header.addEventListener('click', () => {
+        
+        // get the next sibling element of menu-header, which is dropdown-content div
+        const dropdownContent = header.nextElementSibling;
+
+        // Add show-dropdown class when the header is toggled
+        dropdownContent.classList.toggle('show-dropdown');
+       
+      });
+    });
+
+
