05602b5a5cd79cee5a605ceb7dda06f270813405
Jiwon Suh <suhjiwon1995@gmail.com>
2023-10-30 12:14:02 -0400

    deployyyyyyyyyy

--- URBN-portfolio-website-JiwonSuh/vue-urbn-portfolio/deploy.sh
+++ URBN-portfolio-website-JiwonSuh/vue-urbn-portfolio/deploy.sh
@@ -15,11 +15,14 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+git push -f git@github.com:suhjiwon95.github.io/URBN-portfolio-website-JiwonSuh.git main:gh-pages
+
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
 
 cd -s
--- URBN-portfolio-website-JiwonSuh/vue-urbn-portfolio/vite.config.js
+++ URBN-portfolio-website-JiwonSuh/vue-urbn-portfolio/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/URBN-portfolio-website-JiwonSuh/',
+  base: '/suhjiwon95.github.io/',
   plugins: [
     vue(),
   ],
