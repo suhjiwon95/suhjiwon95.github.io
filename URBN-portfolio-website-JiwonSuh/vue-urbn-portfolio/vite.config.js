61da14998464222c9b0ab92804e24f3bb47eca10
Jiwon Suh <suhjiwon1995@gmail.com>
2023-10-30 13:36:26 -0400

    deleted base

--- URBN-portfolio-website-JiwonSuh/vue-urbn-portfolio/vite.config.js
+++ URBN-portfolio-website-JiwonSuh/vue-urbn-portfolio/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/suhjiwon95.github.io/URBN-portfolio-website-JiwonSuh/vue-urbn-portfolio/dist/',
+  
   plugins: [
     vue(),
   ],
